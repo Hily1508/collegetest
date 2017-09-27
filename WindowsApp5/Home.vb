7584dc3b02cfdcaaea715b48e8a56d80e88fd3dc
Gareth Hill <043416@student.warrington.ac.uk>
2017-09-27 10:47:09 +0100

    some changes

--- WindowsApp5/Home.vb
+++ WindowsApp5/Home.vb
@@ -3,6 +3,6 @@
     Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
         Me.Hide()
         frmResult.Show()
-        frmResult.txtResult.Text = password
+        frmResult.txtResult.Text = password2
     End Sub
 End Class
